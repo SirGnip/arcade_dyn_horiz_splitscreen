f1c92b032e68a4603490bd13e182997fa18dfcb3
SirGnip <SirGnip@users.noreply.github.com>
2025-04-07 17:13:32 -0500

    Two cameras

--- dyn_splitscreen.py
+++ dyn_splitscreen.py
@@ -1,5 +1,5 @@
 """
-Goal: Have basic scrolling working with flying ship
+Goal: Independent scrolling with two separate views
 """
 import arcade
 
@@ -12,10 +12,9 @@ class Game(arcade.Window):
         self.actor.position = (100, 100)
         self.sprites.append(self.actor)
 
-    def draw_scene(self, mode):
-        bg = arcade.color.YELLOW if mode == 0 else arcade.color.ORANGE
+    def draw_scene(self):
         arcade.draw_lrbt_rectangle_filled(-10000, 10000, -10000, 10000, arcade.color.BLACK)
-        arcade.draw_lrbt_rectangle_filled(-200, 200, -200, 200, bg)
+        arcade.draw_lrbt_rectangle_filled(-200, 200, -200, 200, arcade.color.YELLOW)
         arcade.draw_circle_filled(0, 0, 30, arcade.color.RED)
         arcade.draw_circle_filled(0, 0, 10, arcade.color.GRAY)
         arcade.draw_circle_filled(300, 300, 20, arcade.color.PINK)
@@ -26,13 +25,16 @@ class Game(arcade.Window):
 
     def on_draw(self):
         self.clear(arcade.color.DARK_BROWN)
-        with self.cam.activate():
-            self.draw_scene(0)
+        with self.cam1.activate():
+            self.draw_scene()
+        with self.cam2.activate():
+            self.draw_scene()
 
     def on_update(self, delta_time):
         self.actor.center_x += self.actor.change_x
         self.actor.center_y += self.actor.change_y
-        scroll_view(self.cam, self.actor)
+        scroll_view(self.cam1, self.actor)
+        scroll_view(self.cam2, self.actor)
 
     def on_key_press(self, symbol: int,modifiers: int):
         d = 5
@@ -55,6 +57,9 @@ class Game(arcade.Window):
             self.actor.angle = 270
             self.actor.change_x = -d
             self.actor.change_y = 0
+        elif symbol == arcade.key.SPACE:
+            self.actor.change_x = 0
+            self.actor.change_y = 0
 
 def scroll_view(cam, actor):
     # translate the screen view to world space
@@ -74,10 +79,16 @@ def scroll_view(cam, actor):
 
 
 if __name__ == '__main__':
-    win = Game(400, 400, "scrolling")
-    win.cam = arcade.Camera2D(
-        projection=arcade.rect.LRBT(0, 400, 0, 400),
+    width, height = 900, 400
+    win = Game(width, height, "scrolling")
+    win.cam1 = arcade.Camera2D(
+        projection=arcade.rect.LBWH(0, 0, width, height),
         position=(0, 0),
-        scissor=arcade.rect.LRBT(50, 350, 50, 350)  # screenspace
+        scissor=arcade.rect.LBWH(50, 80, 350, 300)  # screenspace
+    )
+    win.cam2 = arcade.Camera2D(
+        projection=arcade.rect.LBWH(0, 0, width, height),
+        position=(0, 0),
+        scissor=arcade.rect.LBWH(450, 10, 150, 350)  # screenspace
     )
     win.run()
