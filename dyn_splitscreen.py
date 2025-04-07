b43fe442c469457ba007aca2dbba60f4afef0493
SirGnip <SirGnip@users.noreply.github.com>
2025-04-07 17:11:15 -0500

    Have camera scroll with player movement

--- dyn_splitscreen.py
+++ dyn_splitscreen.py
@@ -1,9 +1,7 @@
 """
 Goal: Get basic scrolling working
-- x have one camera,  a sub-camera (set camera explicitly
-- x draw numbers in global
-- x have actor that can move
-- x scroll view with actor
+- x have player start in center of screen move in all directions
+- x have simple scroll screen behavior when player gets to edge
 """
 import arcade
 
@@ -13,6 +11,7 @@ class Game(arcade.Window):
         super().__init__(w, h, title)
         self.sprites = arcade.SpriteList()
         self.actor = arcade.Sprite(":resources:/images/space_shooter/playerShip1_blue.png")
+        self.actor.position = (100, 100)
         self.sprites.append(self.actor)
 
     def draw_scene(self, mode):
@@ -34,22 +33,36 @@ class Game(arcade.Window):
             self.draw_scene(0)
 
     def on_key_press(self, symbol: int,modifiers: int):
-        d = 20
+        d = 25
         if symbol == arcade.key.ESCAPE:
             self.close()
 
         if symbol == arcade.key.W:
             self.actor.center_y += d
-            self.cam.position += (0, d)
         elif symbol == arcade.key.S:
             self.actor.center_y += -d
-            self.cam.position += (0, -d)
         elif symbol == arcade.key.D:
             self.actor.center_x += d
-            self.cam.position += (d, 0)
         elif symbol == arcade.key.A:
             self.actor.center_x += -d
-            self.cam.position += (-d, 0)
+        print(self.actor.position)
+        scroll_view(self.cam, self.actor)
+
+def scroll_view(cam, actor):
+    view_in_world_space = cam.scissor.move(cam.position[0], cam.position[1])
+    new_view = None  # world-space
+    if actor.center_x < view_in_world_space.left:
+        # shift the world space view
+        new_view = view_in_world_space.align_left(actor.center_x)
+    if actor.center_x > view_in_world_space.right:
+        new_view = view_in_world_space.align_right(actor.center_x)
+    if actor.center_y > view_in_world_space.top:
+        new_view = view_in_world_space.align_top(actor.center_y)
+    if actor.center_y < view_in_world_space.bottom:
+        new_view = view_in_world_space.align_bottom(actor.center_y)
+    if new_view:
+        # offset the camera position to represent the new view in the given screen position
+        cam.position = new_view.left - cam.scissor.left, new_view.bottom - cam.scissor.bottom
 
 
 if __name__ == '__main__':
@@ -59,5 +72,4 @@ if __name__ == '__main__':
         position=(0, 0),
         scissor=arcade.rect.LRBT(50, 350, 50, 350)  # screenspace
     )
-    win.cam.position -= (50, 50)  # offset camera to account for screenspace adjustment
     win.run()
